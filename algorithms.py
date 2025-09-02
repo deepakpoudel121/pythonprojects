6eaf23848794973ca051ce61fbcb848065a403b1
Deepak <thegreatdeepakpoudel@gmail.com>
2025-09-02 11:16:25 +0545

    Algorithms

---
+++ algorithms.py
@@ -0,0 +1,37 @@
+#Duplicate Values
+
+def no_duplicate(nums):
+    if len(set(nums)) == len(nums):
+        return True
+    else:
+        return False
+    
+
+#Given an array nums containing n distinct numbers in the range [0, n] 
+# return the only number in the range that is missing from the array
+# example => [3,0,1] == returns 2
+
+def missing_number(nums):
+    n = len(nums)
+    sum = n*(n+1) / 2
+    total = sum(nums)
+    return sum - total
+
+# 28 min https://www.youtube.com/watch?v=lvO88XxNAzs
+
+
+# Two sum
+
+
+def two_sum(nums, target):
+    heap_map = {}
+    for i, num in enumerate(nums):
+        if target - num in heap_map:
+            return [heap_map[target-num], i]
+        else:
+            heap_map[num] = i
+
+
+def two_sum(nums, target):
+    setnums = set(nums)
+
