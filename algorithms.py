9bca1db791d2bfb1b5d0c52039400e88ecfef16c
Deepak <thegreatdeepakpoudel@gmail.com>
2025-09-03 11:21:33 +0545

    Minimum time and distance between two points

--- algorithms.py
+++ algorithms.py
@@ -73,3 +73,28 @@ def small_number(nums):
     
 nums = [8,1,2,2,3]
 print(small_number(nums))
+
+#Minimum time visiting all points
+#On a 2D plane, there are n points with integer coordinates points[i] = [xi, yi].Return the minimum time in seconds to visit all points in the order given by points.
+#You can move according to the next rules:  
+#In 1 second, you can either:
+#move vertically by one unit,   
+#move horizontally by one unit, or
+#move diagonally (it means to move one unit vertically and one unit horizontally in 1 second).
+#You have to visit the points in the same order as they appear in the array. You are allowed to pass through points that appear later in the order, but these do not count as visits.
+#Example => Input: points = [[1,1],[3,4],[-1,0]] Output: 7
+
+
+def min_time(points):
+    time = 0
+    x1,y1 = points.pop()
+    while points:
+        x2,y2 = points.pop()
+        dist = max(abs(x2-x1), abs(y2-y1))
+        time += dist
+        x1,y1 = x2,y2
+    return time
+    
+
+pints = [[1,1],[3,4],[-1,0]]
+print(min_time(pints))
